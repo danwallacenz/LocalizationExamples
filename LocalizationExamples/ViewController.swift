b8e4864da5557acf3d312c99180d471f7f003fa2
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-01-28 22:12:43 +1300

    add labels, data and image. Format them.

--- LocalizationExamples/ViewController.swift
+++ LocalizationExamples/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        populateValues()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
-
+    func populateValues() {
+        textLabel.text = "Good Morning"
+        numberLabel.text = numberFormatter.stringFromNumber(9999999.999)
+        currencyLabel.text = currencyFormatter.stringFromNumber(50000)
+        dateLabel.text = dateFormatter.stringFromDate(NSDate())
+        imageView.image = UIImage(named: "hello")
+    }
+    
+    var numberFormatter: NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        return formatter
+    }
+    
+    var currencyFormatter: NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        return formatter
+    }
+    
+    var dateFormatter: NSDateFormatter {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .MediumStyle
+        return formatter
+    }
 }
 
